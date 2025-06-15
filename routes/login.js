0898dbbedf99667f73b8f2f876a54e9be822b318
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-15 21:57:26 +0530

    fix: correct middleware log message in login route

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 route.use((req, res, next) => {
-  console.log("Index middleware");
+  console.log("Login middleware");
   next();
 });
 
