93b519df6d552025b12daf9900797531e30b3339
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-15 22:18:28 +0530

    feat: implement login functionality with database query

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = express.Router();
+const pool = require("../db");
 
 route.use((req, res, next) => {
   console.log("Login middleware");
@@ -8,6 +9,27 @@ route.use((req, res, next) => {
 
 route.get("/", (req, res) => {
   res.render("login");
+});
+
+route.post("/", (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+
+  const sql = "SELECT * FROM users WHERE uemail = ? AND upass = ?";
+
+  pool.query(sql, [email, password], (err, results, fields) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log(results);
+
+    if (results.length > 0) {
+      res.send(`Welcome, ${results[0].uname}. Login successful.`);
+    } else {
+      res.send("Invalid email or password.");
+    }
+  });
 });
 
 module.exports = route;
