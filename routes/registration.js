0bd1abf5a854bcb38dc612303dfd8431e469c662
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-15 22:18:36 +0530

    feat: implement user registration route with database insertion

--- routes/registration.js
+++ routes/registration.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = express.Router();
+const pool = require("../db");
 
 route.use((req, res, next) => {
   console.log("Registration middleware");
@@ -8,6 +9,25 @@ route.use((req, res, next) => {
 
 route.get("/", (req, res) => {
   res.render("registration");
+});
+
+route.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+  const val = [name, email, password];
+  console.log(req.body);
+
+  const sql = "INSERT INTO users (uname, uemail, upass) VALUES (?, ?, ?)";
+
+  pool.query(sql, val, (err, result, fields) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    console.log("User registered:", result.insertId);
+    res.redirect("/login");
+  });
+
+  // res.redirect("/login");
 });
 
 module.exports = route;
