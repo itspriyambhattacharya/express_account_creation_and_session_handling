954129d8c43876fee313ccbdfec7c41792344fde
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-15 21:53:00 +0530

    feat: add database connection pool configuration

---
+++ db.js
@@ -0,0 +1,9 @@
+const mysql2 = require("mysql2");
+const pool = mysql2.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "mysql12345678",
+  database: "user_auth",
+  connectionLimit: 10,
+});
