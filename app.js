a3aaadec9a5ae505bb868b7bbc5f16d67943a5a0
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-15 22:30:16 +0530

    feat: add session management middleware for user sessions

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "localhost123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
 const idx = require("./routes/index");
 const reg = require("./routes/registration");
